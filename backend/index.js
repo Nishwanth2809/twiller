7330e2b1c35510b413c7a1f6bdb9d41c80a30f36
Nishwanth2809 <nishwanthyarra@gmail.com>
2026-05-05 12:55:13 +0530

    Support FIREBASE_SERVICE_ACCOUNT env var for production

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,12 @@ const require = createRequire(import.meta.url);
 
 if (!admin.apps.length) {
   try {
-    const serviceAccount = require("./serviceAccountKey.json");
+    let serviceAccount;
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+      serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    } else {
+      serviceAccount = require("./serviceAccountKey.json");
+    }
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
